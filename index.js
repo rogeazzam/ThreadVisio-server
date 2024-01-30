4f9511c9c5dea120a87ba922dbdf8fedadf7f787
rogeazzam <roge_azzam@hotmail.com>
2024-01-30 18:24:55 +0200

    Connecting to MongoDB database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-
+const {mongoose} = require('mongoose');
 const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Database connected'))
+.catch((err) => console.log('Database not connected', err))
 
 app.use('/', require('./routes/authRoutes'));
 
